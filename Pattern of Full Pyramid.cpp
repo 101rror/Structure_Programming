826b801240ab1135c9f73897f48d14269bf1517c
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-01-11 14:27:44 +0600

    Add files via upload

---
+++ Pattern of Full Pyramid.cpp
@@ -0,0 +1,68 @@
+///Pattern of Full Pyramid
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;                               //101rror
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL))
+
+#define T while(t--)
+#define for2(i,a,b) for(i=a;i>=b;i--)
+#define for3(i,a,b) for(i=a;i<=b;i=i+2)
+#define for1(i,a,b) for(i=a;i<=b;i=i+1)
+#define for4(i,a,b) for(i=a;i>=b;i=i-2)
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    fastread();
+
+    printf("Enter a Number : ");
+
+    int num, k;
+    cin >> num;
+    cout << endl;
+
+    for(int i = 1; i <+ num; i++, k = 0)
+    {
+        for(int j = 1; j <= (num - i); j++)
+        {
+            cout << " ";
+        }
+        while( k != (2 * i - 1))
+        {
+            cout << "*";
+            k++;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+
+    return 0;
+}
